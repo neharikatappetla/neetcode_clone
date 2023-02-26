6965928f05e6d2fe70af25bf2c406740fe7529fd
20WH1A0582 TAPPETLA NEHARIKA <20wh1a0582@bvrithyderabad.edu.in>
2023-02-26 10:12:34 +0530

    completed hello world app

---
+++ server.js
@@ -0,0 +1,14 @@
+const express =require("express");
+
+const app = express();
+app.get("/",function(req,res){
+    res.send("Hello World");
+})
+
+app.get("/myName",function(req,res){
+    res.send("/Neharika");
+})
+
+app.listen(3000,function(){
+    console.log("Server running on http://localhost:3000");
+})
