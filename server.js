f09467928e37bc0eea6ea7f4cb1e5e98a6ba7bcf
20WH1A0582 TAPPETLA NEHARIKA <20wh1a0582@bvrithyderabad.edu.in>
2023-02-26 14:15:27 +0530

    mongodb connection

--- server.js
+++ server.js
@@ -1,6 +1,30 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+
+
+const str = process.env.MONGO_CONNECTION_STRING;
+
 const express =require("express");
 
 const app = express();
+
+const mongoose = require("mongoose");
+
+mongoose.set('strictQuery', true);
+mongoose.connect(process.env.MONGO_CONNECTION_STRING,function(err){
+    if(err)
+        console.error(err);
+    else{
+        console.log("DB Connection Successful");
+
+        app.listen(3000,function(){
+            console.log("Server running on http://localhost:3000");
+        });
+
+    }
+})
+
 
 app.use(express.static("frontend"))
 
@@ -8,6 +32,3 @@ app.get("/",function(req, res){
     res.sendFile(__dirname +"/frontend/index.html")
 })
 
-app.listen(3000,function(){
-    console.log("Server running on http://localhost:3000");
-})
